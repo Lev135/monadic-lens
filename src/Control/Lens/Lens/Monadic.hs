f256fb80df1d52325b379e4ab0b5f4c4086354e7
Lev Dvorkin <lev_135@mail.ru>
2023-06-10 21:44:10 +0300

    Rename MfaAms to FunctorM

--- src/Control/Lens/Lens/Monadic.hs
+++ src/Control/Lens/Lens/Monadic.hs
@@ -4,21 +4,24 @@ import Control.Applicative (Const(..))
 import Control.Monad.Identity (Identity(..))
 import Data.Functor.Compose (Compose(..))
 
-class Functor f => MfaAms f where
-  mfaAms :: forall m a s. Monad m => m (f a) -> (a -> m s) -> m (f s)
+{-
+  law> fmap h = runIdentity . gfmap (Identity . h) . Identity
+-}
+class Functor f => FunctorM f where
+  fmapM :: forall m b t. Monad m => (b -> m t) -> m (f b) -> m (f t)
 
-instance MfaAms (Const a) where
-  mfaAms mfa _ = Const . getConst <$> mfa
+instance FunctorM (Const a) where
+  fmapM _ c = Const . getConst <$> c
 
-instance MfaAms Identity where
-  mfaAms mfa ams = Identity <$> (mfa >>= (ams . runIdentity))
+instance FunctorM Identity where
+  fmapM bmt mb = Identity <$> (mb >>= (bmt . runIdentity))
 
 type LensM m s t a b =
-  forall f. MfaAms f =>
+  forall f. FunctorM f =>
     (a -> Compose m f b) -> s -> Compose m f t
 
 type LensM' m s a = LensM m s s a a
 
 lensM :: Monad m => (s -> m a) -> (s -> b -> m t) -> LensM m s t a b
 lensM getter setter f s =
-  Compose $ (getter s >>= getCompose . f) `mfaAms` setter s
+  Compose $ setter s `fmapM` (getter s >>= getCompose . f)
