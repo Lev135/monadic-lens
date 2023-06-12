4dd56aa912f9d222a387e3fe8e8516852a73788a
Lev Dvorkin <lev_135@mail.ru>
2023-06-12 17:36:19 +0300

    Full polymorphic Setter added

--- src/Control/Lens/Setter/Monadic.hs
+++ src/Control/Lens/Setter/Monadic.hs
@@ -1,14 +1,20 @@
 module Control.Lens.Setter.Monadic where
-import Control.Monad.Identity
+
+import Control.Lens.Internal.Setter (Settable(untainted))
+import Control.Monad.Identity (Identity(..))
 import Data.Functor.Compose (Compose(..))
+
+type SetterM m s t a b =
+  forall f. Settable f =>
+    (a -> Compose m f b) -> s -> Compose m f t
 
 type ASetterM m s t a b =
   (a -> Compose m Identity b) -> s -> Compose m Identity t
 
 type ASetterM' m s a = ASetterM m s s a a
 
-setsM :: Functor m => ((a -> m b) -> s -> m t) -> ASetterM m s t a b
-setsM setter f = Compose . fmap Identity . setter (fmap runIdentity . getCompose . f)
+settingM :: Functor m => ((a -> m b) -> s -> m t) -> SetterM m s t a b
+settingM setter f = Compose . fmap pure . setter (fmap untainted . getCompose . f)
 
 overM :: Functor m => ASetterM m s t a b -> (a -> m b) -> s -> m t
 overM l h = fmap runIdentity . getCompose . l (Compose . fmap Identity . h)
